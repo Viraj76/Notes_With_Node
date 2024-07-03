8d943402b576534da867b1374e2ddeb8ad972b02
Viraj76 <virajg.7680@gmail.com>
2024-07-03 17:36:52 +0530

    initial commits

---
+++ app/src/main/java/com/appsv/noteswithnode/presentation/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.appsv.noteswithnode.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.appsv.noteswithnode.presentation.ui.theme.NotesWithNodeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        installSplashScreen()
+        setContent {
+            NotesWithNodeTheme {
+
+            }
+        }
+    }
+}
+
